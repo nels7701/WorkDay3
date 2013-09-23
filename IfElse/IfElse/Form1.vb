104860409415a7826e084bf8dca4884bbda1ff69
idsc3101 <idsc3101@gmail.com>
2013-09-23 17:24:07 -0500

    Worked on Nested ifs

--- IfElse/IfElse/Form1.vb
+++ IfElse/IfElse/Form1.vb
@@ -3,15 +3,30 @@
 Public Class Form1
 
     Private Sub btnFindLarger_Click(sender As System.Object, e As System.EventArgs) Handles btnFindLarger.Click
-        Dim a, b, larger As Double
+        
+        If Not (IsNumeric(txtFirstNum.Text) And IsNumeric(txtSecondNum.Text)) Then
+            MessageBox.Show("Error: Please type only Positive numbers!")
+            Return
+        End If
+
+        Dim a, b As Double
         a = CDbl(txtFirstNum.Text)
         b = CDbl(txtSecondNum.Text)
-        If a > b Then
-            larger = a
-        Else
-            larger = b
+
+        If (a < 0 Or b < 0) Then
+            MessageBox.Show("Error: Please type only Positive numbers!")
+            Return
         End If
-        txtResult.Text = "the larger number is " & CStr(larger)
+        'Sterile Area
+
+        If a > b Then
+            txtResult.Text = "the larger number is " & CStr(a)
+        ElseIf b > a Then
+            txtResult.Text = "the larger number is " & CStr(b)
+        Else
+            txtResult.Text = "The two are equal."
+        End If
+
     End Sub
 
     Private Sub Form1_Load(sender As System.Object, e As System.EventArgs) Handles MyBase.Load
